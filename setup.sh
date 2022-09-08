2793643d977ff11422b237e6a4757eebb8c10ab3
jshou <joshuahou@gmail.com>
2022-09-08 13:54:57 -0700

    include nvim symlinking

--- setup.sh
+++ setup.sh
@@ -3,5 +3,16 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+# ln -s ~/.vim/vimrc ~/.vimrc
+# ln -s ~/.vim/gvimrc ~/.gvimrc
+
+nvim_config="$HOME/.config/nvim/init.vim"
+mkdir -p ~/.config/nvim
+
+if [ -e $nvim_config ]
+then
+    printf "File already exists: $target, backing up"
+    mv "$nvim_config" "$nvim_config.backup"
+fi
+
+ln -s "$PWD/nvim_init.vim" "$nvim_config"
